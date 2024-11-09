2bc165f20b88e4f6faa6a3f68cef50dd5fe765b1
KadeSN21 <116524466+KadeSN21@users.noreply.github.com>
2024-11-08 21:24:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,23 @@
-function permutationSort(a) {
-    if (a.length === 0) {
-        return 1;
+function permutationSort(arr) {
+    if (arr.length === 0) {
+        return 1; 
     }
-    const permutations = permute(a);
+
     let count = 0;
+    const permutations = permute(arr);  
 
     for (let perm of permutations) {
         count++;
         if (isSorted(perm)) {
-            for (let i = 0; i < a.length; i++) {
-                a[i] = perm[i];
+            for (let i = 0; i < arr.length; i++) {
+                arr[i] = perm[i];
             }
-            return count;
+            return count;  
         }
     }
-}
 
+    return count;  
+}
 
 function isSorted(arr) {
     for (let i = 1; i < arr.length; i++) {
@@ -26,20 +28,22 @@ function isSorted(arr) {
     return true;
 }
 
-function permute(arr, start = 0) {
-    if (start === arr.length - 1) {
-        return [[...arr]];
-    }
-
+function permute(arr) {
     let result = [];
-    let seen = new Set();
-    for (let i = start; i < arr.length; i++) {
-        if (!seen.has(arr[i])) {
-            [arr[start], arr[i]] = [arr[i], arr[start]];
-            result = result.concat(permute(arr, start + 1));
-            [arr[start], arr[i]] = [arr[i], arr[start]];
-            seen.add(arr[i]);
+    
+    if (arr.length === 1) {
+        return [arr.slice()];
+    }
+    
+    for (let i = 0; i < arr.length; i++) {
+        let rest = arr.slice(0, i).concat(arr.slice(i + 1));
+        let restPerms = permute(rest);
+
+        for (let perm of restPerms) {
+            result.push([arr[i], ...perm]);  
         }
     }
     return result;
 }
+
+
